787e1ca060cf639d3673d94434182357b4683a9a
SparkleFox715 <jupiter.am705@gmail.com>
2021-05-27 22:22:10 -0500

    pushed

--- src/main/java/frc/robot/subsystems/TurretSys.java
+++ src/main/java/frc/robot/subsystems/TurretSys.java
@@ -32,9 +32,8 @@ public class TurretSys extends SubsystemBase {
 		return turret.getSelectedSensorPosition();
 	}
 	public void resetTurret(){
-		double tolerance = 5;
-		if(Math.abs(getDegrees())>tolerance){
-			setTurret(Constants.KP_ROT_TURRET*((getDegrees()-200)/100)+Constants.CONSTANT_FORCE_TURRET);
+		if(Math.abs(getDegrees())>200){
+			setTurret(Constants.KP_ROT_TURRET*((getDegrees())/100)+Constants.CONSTANT_FORCE_TURRET);
 		}
 	}
 	@Override
